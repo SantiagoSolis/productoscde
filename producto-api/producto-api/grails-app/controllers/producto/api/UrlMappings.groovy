b1566dcb70d4acc87e6e314d3518bef955428390
Santiago <sant.solis.luna@gmail.com>
2019-06-24 20:06:55 -0300

    se progresa con la api

--- producto-api/producto-api/grails-app/controllers/producto/api/UrlMappings.groovy
+++ producto-api/producto-api/grails-app/controllers/producto/api/UrlMappings.groovy
@@ -3,12 +3,14 @@ package producto.api
 class UrlMappings {
 
     static mappings = {
-        "/$controller/$action?/$id?(.$format)?"{
+
+        "/$controller/$action?/$id?(.$format)?"
+                {
             constraints {
                 // apply constraints here
             }
         }
-
+        "/show/$id"(controller: 'product', action: 'show')
         "/"(view:"/index")
         "500"(view:'/error')
         "404"(view:'/notFound')
