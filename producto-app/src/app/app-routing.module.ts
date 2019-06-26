23377adc5efd5e0002c52e36395197101720f1bc
Santiago <sant.solis.luna@gmail.com>
2019-06-26 14:50:38 -0300

    se actualizo la pagina principal de la app

--- producto-app/src/app/app-routing.module.ts
+++ producto-app/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'show', loadChildren: './show/show/show.module#ShowPageModule' },
+  { path: 'show', loadChildren: './show/show.module#ShowPageModule' },
 ];
 
 @NgModule({
--- producto-app/src/app/home/home.page.ts
+++ producto-app/src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { ApiproductoService } from '../../services/apiproducto.service';
+import { Observable } from "rxjs";
+import { map,catchError } from "rxjs/operators";
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
 export class HomePage {
 
-  constructor() {}
+  listProduct :any;
+  product:any;
 
+  constructor(private apiProducto:ApiproductoService) {
+     this.listProduct = this.getProducts();
+  }
+
+  getProducts(){
+    this.apiProducto.getProducts().
+      subscribe(products => {
+        console.log(products);
+        this.listProduct=products;
+      });
+  }
+  mostrarDetalleProducto(id)
+  {
+    this.apiProducto.getById(id.toString()).
+    subscribe(product => {
+      console.log(product);
+      this.product=product;
+    });
+  }
 }
